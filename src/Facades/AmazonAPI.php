a86b49b6dbb24ab876accde7ee72478116293f44
TheCodeBunny <thecodebunny@gmail.com>
2023-04-03 16:52:00 +0200

    Changed to Version 5

---
+++ src/Facades/AmazonAPI.php
@@ -0,0 +1,17 @@
+<?php
+namespace TheCodeBunny\PaApi\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class AmazonAPI extends Facade
+{
+	/**
+	 * Get the registered name of the component.
+	 *
+	 * @return string
+	 */
+	protected static function getFacadeAccessor()
+	{
+		return 'AmazonPaapi5';
+	}
+}
---
+++ src/LaravelAmazonPaapi5.php
@@ -0,0 +1,9 @@
+<?php
+namespace TheCodeBunny\PaApi;
+
+use TheCodeBunny\PaApi\com\amazon\paapi5\v1\api\DefaultApi;
+
+class PaApi extends DefaultApi
+{
+    
+}
